3e9d65bc42829ccb48dd0a756498ab04a521cd7d
Shawn Schwartz <shawn.t.schwartz@gmail.com>
2017-11-09 22:09:48 -0800

    testing with new php templates

---
+++ experiment/src/resumes_html/BLS_GroupOne/NW_M_UCB.php
@@ -0,0 +1,25 @@
+<html>
+  <head>
+    <title>NW_M_UCB</title>
+    <link rel="stylesheet" href="../../css/jspsych.css">
+  </head>
+  <body>
+    <div class="resumeContainer">
+      <center>
+        <img src="../../stimuli/NW_M_JohnCurtis.jpg" />
+      </center>
+      <center>
+        <h1>JOHN CURTIS</h1>
+        <h3 style="margin-top: -15px;">48 Frederick St., Apartment 203 Cambridge, MA 02138</h3>
+        <p style="margin-top: -20px; margin-bottom: -10px;"><strong>jcooper@hls.harvard.edu &bull; 978-555-4422</strong></p>
+      </center>
+      <br /><br />
+      <strong style="font-size: 22px;">EDUCATION</strong>
+      <br />
+      <strong>Harvard Law School</strong>, Cambridge, MA, J.D., June 2017
+      <br />Activies: Harvard Law Students Association, <em>Co-President</em>, Harvard Human Rights Journal, <em>Member</em>
+      <br /><br />
+      <?php include('UCB_template.php'); ?>
+    </div>
+  </body>
+</html>
