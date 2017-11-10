668c803f8906ad772c047a2d0ec2e2487542c8ac
Shawn Schwartz <shawn.t.schwartz@gmail.com>
2017-11-09 23:56:57 -0800

    attempting to fix dir issues for php temps

--- experiment/src/resumes/BLS_Group_1/NW_F_STAN.php
+++ experiment/src/resumes/BLS_Group_1/NW_F_STAN.php
@@ -1,13 +1,13 @@
 <html>
   <head>
     <title>NW_F_STAN</title>
-    <?php include('header_template.php'); ?>
+    <?php include('../header_template.php'); ?>
   </head>
   <body>
     <div class="container">
       <?php
-        include('NW_F_JaneConner_template.php');
-        include('STAN_template.php');
+        include('../NW_F_JaneConner_template.php');
+        include('../STAN_template.php');
       ?>
     </div>
   </body>
